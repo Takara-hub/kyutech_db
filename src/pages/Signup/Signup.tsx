7e797f86e010ca78cdb3231e295dc559987ab675
TaiseiIkeda <tai11sei0514@gmail.com>
2022-06-16 20:08:05 +0900

    :+1: ファイルの追加

---
+++ src/pages/Signup/Signup.tsx
@@ -0,0 +1,12 @@
+import PrimaryButton from "../../components/PrimaryButton";
+
+const Signup = () => {
+  return (
+    <div>
+      Hello World!
+      <PrimaryButton></PrimaryButton>
+    </div>
+  );
+};
+
+export default Signup;
