a42f6727c053068891886488f5f065b65c7320d1
Takara Ishimoto <57268381+IshimotoTakara@users.noreply.github.com>
2022-06-19 19:53:17 +0900

    Revert "Feature/6"

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Top from "./pages/Top/Top";
 import Login from "./pages/Login/Login";
-import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route index element={<Top />} />
         <Route path="login" element={<Login />} />
-        <Route path="Signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
