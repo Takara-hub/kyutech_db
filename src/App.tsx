43f4d893ba5dc81209b5bcb3934234058f6e5154
TaiseiIkeda <tai11sei0514@gmail.com>
2022-06-19 18:04:57 +0900

    Signフォームの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Top from "./pages/Top/Top";
 import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route index element={<Top />} />
         <Route path="login" element={<Login />} />
+        <Route path="Signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
